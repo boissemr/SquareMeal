a87f1a21fe61579e48785effc3d0bd0bc750a008
Matt Boisseau <boissemr@miamioh.edu>
2016-03-29 18:07:15 -0400

    set up menu infrastructure and basic title screen
    MenuSystem manages MenuObjects, which can be moved and faded. Open-Sans
    is now included.

---
+++ Assets/scripts/MenuObject.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuObject : MonoBehaviour {
+
+	public MenuSystem	menuSystem;
+	public bool			lockPosition,
+						lockRotation,
+						lockScale;
+
+	[HideInInspector] public Vector3	targetPosition;
+
+	Text[] textSubObjects;
+	Image[] imageSubObjects;
+
+	float opacity;
+
+	void Start() {
+		textSubObjects = GetComponentsInChildren<Text>();
+		imageSubObjects = GetComponentsInChildren<Image>();
+		targetPosition = transform.position;
+	}
+
+	void Update() {
+		if(!lockPosition) {
+			transform.position = Vector3.Lerp(transform.position, targetPosition, menuSystem.menuLerpSpeed * Time.deltaTime);
+		}
+	}
+
+	public void fadeTo(float opacity, float duration) {
+		foreach(Text o in textSubObjects) {
+			o.CrossFadeAlpha(opacity, duration, false);
+		}
+		foreach(Image o in imageSubObjects) {
+			o.CrossFadeAlpha(opacity, duration, false);
+		}
+	}
+
+	public void setRaycastTarget(bool state) {
+		foreach(Text o in textSubObjects) {
+			o.raycastTarget = false;
+		}
+		foreach(Image o in imageSubObjects) {
+			o.raycastTarget = false;
+		}
+	}
+}
---
+++ Assets/scripts/MenuSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSystem : MonoBehaviour {
+
+	public MenuObject	title,
+						mainMenu,
+						tapToBeginText;
+	public float		menuLerpSpeed,
+						menuTransitionDuration;
+
+	void Start() {
+	}
+
+	public void tapToBegin() {
+		mainMenu.targetPosition = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0));
+		title.targetPosition = Camera.main.ViewportToScreenPoint(new Vector3(0.25f, 0.5f, 0));
+		tapToBeginText.fadeTo(0, menuTransitionDuration);
+		tapToBeginText.setRaycastTarget(false);
+	}
+}
