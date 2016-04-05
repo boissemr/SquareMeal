a4651e02ac4b69466be5e396fe57ebad91978ed9
Matt Boisseau <boissemr@miamioh.edu>
2016-04-05 17:16:01 -0400

    added new game option
    new game now closes main menu and opens game area; MenuSystem refactored
    to utilize coroutines

--- Assets/scripts/MenuObject.cs
+++ Assets/scripts/MenuObject.cs
@@ -4,45 +4,33 @@ using UnityEngine.UI;
 
 public class MenuObject : MonoBehaviour {
 
-	public MenuSystem	menuSystem;
-	public bool			lockPosition,
-						lockRotation,
-						lockScale;
+	float		opacity;
+	Text[]		textSubObjects;
+	Image[] 	imageSubObjects;
 
-	[HideInInspector] public Vector3	targetPosition;
-
-	Text[] textSubObjects;
-	Image[] imageSubObjects;
-
-	float opacity;
-
+	// start
 	void Start() {
 		textSubObjects = GetComponentsInChildren<Text>();
 		imageSubObjects = GetComponentsInChildren<Image>();
-		targetPosition = transform.position;
 	}
 
-	void Update() {
-		if(!lockPosition) {
-			transform.position = Vector3.Lerp(transform.position, targetPosition, menuSystem.menuLerpSpeed * Time.deltaTime);
+	// move to vector3
+	public IEnumerator moveTo(Vector3 targetPosition, float duration) {
+		while(Vector3.Distance(targetPosition, transform.position) > .01f) {
+			transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime / duration);
+			yield return new WaitForEndOfFrame();
 		}
 	}
 
+	// fade to opacity
 	public void fadeTo(float opacity, float duration) {
-		foreach(Text o in textSubObjects) {
-			o.CrossFadeAlpha(opacity, duration, false);
-		}
-		foreach(Image o in imageSubObjects) {
-			o.CrossFadeAlpha(opacity, duration, false);
-		}
+		foreach(Text o in textSubObjects)	o.CrossFadeAlpha(opacity, duration, false);
+		foreach(Image o in imageSubObjects)	o.CrossFadeAlpha(opacity, duration, false);
 	}
 
+	// toggle raycast target
 	public void setRaycastTarget(bool state) {
-		foreach(Text o in textSubObjects) {
-			o.raycastTarget = false;
-		}
-		foreach(Image o in imageSubObjects) {
-			o.raycastTarget = false;
-		}
+		foreach(Text o in textSubObjects)	o.raycastTarget = false;
+		foreach(Image o in imageSubObjects)	o.raycastTarget = false;
 	}
 }
--- Assets/scripts/MenuSystem.cs
+++ Assets/scripts/MenuSystem.cs
@@ -5,17 +5,22 @@ public class MenuSystem : MonoBehaviour {
 
 	public MenuObject	title,
 						mainMenu,
-						tapToBeginText;
-	public float		menuLerpSpeed,
-						menuTransitionDuration;
+						tapToBeginText,
+						titleBackdrop;
+	public float		menuTransitionDuration;
 
-	void Start() {
-	}
-
+	// called when screen tapped on in title screen
 	public void tapToBegin() {
-		mainMenu.targetPosition = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0));
-		title.targetPosition = Camera.main.ViewportToScreenPoint(new Vector3(0.25f, 0.5f, 0));
+		StartCoroutine(title.moveTo(Camera.main.ViewportToScreenPoint(new Vector3(0.25f, 0.5f, 0)), menuTransitionDuration));
+		StartCoroutine(mainMenu.moveTo(Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0)), menuTransitionDuration));
 		tapToBeginText.fadeTo(0, menuTransitionDuration);
 		tapToBeginText.setRaycastTarget(false);
+	}
+
+	// called when new game is selected
+	public void newGame() {
+		titleBackdrop.fadeTo(0, menuTransitionDuration);
+		StartCoroutine(title.moveTo(Camera.main.ViewportToScreenPoint(new Vector3(-1f, 0.5f, 0)), menuTransitionDuration));
+		StartCoroutine(mainMenu.moveTo(Camera.main.ViewportToScreenPoint(new Vector3(2f, 0.5f, 0)), menuTransitionDuration));
 	}
 }
